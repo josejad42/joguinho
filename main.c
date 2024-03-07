7e6a66943ccd03037abb6d0b62eabb476e094d68
Alex Carvalho <jac@cin.ufpe.br>
2024-03-07 00:35:56 -0300

    Adicionando tela em branco

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "raylib.h"
+#include "raymath.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "tela em branco");
+
+
+
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+            ClearBackground(LIGHTGRAY);
+
+        EndDrawing();
+
+    }
+
+   
+    CloseWindow();     
+   
+
+    return 0;
+}
+
