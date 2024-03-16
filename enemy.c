238c87cc99837db9ad72244d4123d24e7cec33fe
Alex Carvalho <jac@cin.ufpe.br>
2024-03-16 20:52:30 -0300

    add enemy

---
+++ enemy.c
@@ -0,0 +1,44 @@
+#include "raylib.h"
+#include "raymath.h"
+#include "enemy.h"
+
+
+Enemy1 inimigo;
+
+void init_enemy(){
+    inimigo.position = (Vector2) {1700, 200};
+    Image enemy_image = LoadImage("assets/lobocorrendo.png");
+    ImageResize(&enemy_image, 1000, 75);
+    inimigo.img = LoadTextureFromImage(enemy_image);
+ 
+    inimigo.currentFrame = 0;
+    inimigo.framesCounter = 0;
+    inimigo.framesSpeed = 10;
+    inimigo.frameRec = (Rectangle) {0.0f, 0.0f, (float)inimigo.img.width/11, (float)inimigo.img.height };
+    inimigo.img.width*=-1;
+}
+
+void mov_enemy(){
+     inimigo.framesCounter++;
+        if (inimigo.framesCounter >= (60/inimigo.framesSpeed)){
+            inimigo.framesCounter = 0;
+            inimigo.currentFrame++;
+
+            if (inimigo.currentFrame > 5) inimigo.currentFrame = 0;
+
+            inimigo.frameRec.x = (float) inimigo.currentFrame*(float)inimigo.img.width/11;
+        }
+
+        inimigo.position.x -= 2;
+        if (inimigo.position.x <= 0){ 
+            inimigo.position.x = 1900;
+        }
+}
+
+void draw_enemy(){
+    DrawTextureRec(inimigo.img, inimigo.frameRec, inimigo.position, WHITE);
+}
+
+void unload_enemy(){
+    UnloadTexture(inimigo.img);
+}
--- main.c
+++ main.c
@@ -1,30 +1,32 @@
 #include "raylib.h"
 #include "raymath.h"
+#include "enemy.h"
 
-int main(void)
-{
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+
+int main(void){
+    const int screenWidth = 1800;
+    const int screenHeight = 1000;
 
     InitWindow(screenWidth, screenHeight, "tela em branco");
 
-
+    init_enemy();
 
     // Main game loop
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()){
+
+        mov_enemy();
 
         BeginDrawing();
 
-            ClearBackground(BLACK);
+            ClearBackground(WHITE);
+            draw_enemy();
 
         EndDrawing();
 
     }
 
-   
+    unload_enemy();
     CloseWindow();     
-   
 
     return 0;
 }
